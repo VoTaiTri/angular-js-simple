df817febe83f9a24078e97563b820ed38a9c70b6
VoTaiTri <vo.tai.tri@framgia.com>
2015-11-02 15:33:42 +0700

    finish learn lesson 3

---
+++ app/controllers/lesson3_controller.rb
@@ -0,0 +1,4 @@
+class Lesson3Controller < ApplicationController
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
 
   get "lesson1" => "lesson1#index"
   get "lesson2" => "lesson2#index"
+  get "lesson3" => "lesson3#index"
 end
