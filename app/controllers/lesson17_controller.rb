388eb9f778678519125b78ef9d9f607d52df343a
VoTaiTri <vo.tai.tri@framgia.com>
2016-07-14 14:25:25 +0700

    Lesson 17 14/7/2016

---
+++ app/controllers/lesson17_controller.rb
@@ -0,0 +1,4 @@
+class Lesson17Controller < ApplicationController
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -17,4 +17,5 @@ Rails.application.routes.draw do
   get "lesson15" => "lesson15#index"
   get "lesson15" => "lesson15#index"
   get "lesson16" => "lesson16#index"
+  get "lesson17" => "lesson17#index"
 end
